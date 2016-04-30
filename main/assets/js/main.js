649f4600b72d195f7eda0d60ab988f32cf563f6f
meghankelly-cartography <mkelly22@wisc.edu>
2016-04-30 12:36:46 -0500

    Slider added, need to connect to data

--- main/assets/js/main.js
+++ main/assets/js/main.js
@@ -1567,3 +1567,21 @@ function filterByPressure(minPressure, maxPressure, data) {
 		return f;
 }
 
+
+
+//Sequencer function, create time line
+$(function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
+
+
